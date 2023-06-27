bb3570c67df757e69b504048844bfa76501b3626
Neor-IT <qweqwe1337228@gmail.com>
2023-06-28 00:16:41 +0300

    Called function from smart contract using abigen

--- internal/service/contracthandler/getAllNFTs.go
+++ internal/service/contracthandler/getAllNFTs.go
@@ -1,55 +1,47 @@
 package contracthandler
 
 import (
-	"context"
 	"encoding/json"
 
+	"GethBackServ/internal/endpoint/abigencontract"
 	"GethBackServ/internal/service/structure"
 
-	"github.com/ethereum/go-ethereum"
-	"github.com/ethereum/go-ethereum/accounts/abi"
+	"github.com/ethereum/go-ethereum/accounts/abi/bind"
 	"github.com/ethereum/go-ethereum/common"
 	"github.com/ethereum/go-ethereum/ethclient"
 )
 
-func callGetAllNFTs(client *ethclient.Client, contractAbi abi.ABI, contractAddress common.Address) ([]interface{}, error) {
-	getAllNFTs, err := contractAbi.Pack("getAllNFTs")
+func callGetAllNFTs(client *ethclient.Client, contractInstance *abigencontract.Main) ([]abigencontract.Nft, error) {
+	result, err := contractInstance.GetAllNFTs(&bind.CallOpts{})
 	if err != nil {
 		return nil, err
 	}
 
-	result, err := client.CallContract(context.Background(), ethereum.CallMsg{
-		To:   &contractAddress,
-		Data: getAllNFTs,
-	}, nil)
-
-	if err != nil {
-		return nil, err
-	}
-
-	return contractAbi.Unpack("getAllNFTs", result)
+	return result, nil
 }
 
-func GetNFTs(client *ethclient.Client, contractAbi abi.ABI, contractAddress common.Address) ([]structure.NFTInfo, error) {
-	data, err := callGetAllNFTs(client, contractAbi, contractAddress)
+func GetNFTs(client *ethclient.Client, contractAddress common.Address) ([]structure.NFTInfo, error) {
+	contractInstance, err := abigencontract.NewMain(contractAddress, client)
+	if err != nil {
+		return nil, err
+	}
+
+	data, err := callGetAllNFTs(client, contractInstance)
 	if err != nil {
 		return nil, err
 	}
 
 	var nftInfoList []structure.NFTInfo
 
-	for i := range data {
-		jsonData, err := json.Marshal(data[i])
+	for _, nft := range data {
+		jsonData, err := json.Marshal(nft)
 		if err != nil {
 			return nil, err
 		}
 
-		jsonData = jsonData[1 : len(jsonData)-1] // delete [ and ] from string
-
 		var nftInfo structure.NFTInfo
 
 		err = json.Unmarshal(jsonData, &nftInfo)
-
 		if err != nil {
 			return nil, err
 		}
